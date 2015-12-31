fbe0bb26b690afa7795ab1ec3048541f08820d44
Tomas Votruba <tomas.vot@gmail.com>
2015-12-31 03:28:35 +0100

    travis: cache composer

---
+++ check-cs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "OK";
+
+# todo: add to coding-standard repo
+# php vendor/bin/phpcs $1 --standard=vendor/symplify/coding-standard/src/SymplifyCodingStandard/ruleset.xml -p -s --colors
+# echo "OK";
+# php vendor/bin/phpcs $1 --standard=PSR2 -p -s --colors
+# echo "OK";
+# php vendor/bin/php-cs-fixer fix $1 --dry-run --diff -v --level=symfony
+# echo "OK";
+
+# sth like
+# vendor/bin/symplify-cs dir

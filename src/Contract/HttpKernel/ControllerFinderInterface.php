1174e6a52a4eab60a5852c272664b20d4275b0ef
Tomas Votruba <tomas.vot@gmail.com>
2016-02-24 00:10:08 +0000

    fix PHP 5.6 compatibility

--- src/Contract/HttpKernel/ControllerFinderInterface.php
+++ src/Contract/HttpKernel/ControllerFinderInterface.php
@@ -14,5 +14,5 @@ interface ControllerFinderInterface
      *
      * @return string[]
      */
-    public function findControllersInDirs(array $dirs) : array;
+    public function findControllersInDirs(array $dirs);
 }
